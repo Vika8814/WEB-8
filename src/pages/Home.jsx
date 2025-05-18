6e87e24e906a76b365aaeee7d09a078b76fc150a
Vika8814 lab1 <mvv081105@gmail.com>
2025-05-18 12:32:46 +0300

    Лабораторна 9: Створила Home сторінку за допомогою MovieList

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+// src/pages/Home.jsx
+import React from 'react';
+import MovieList from '../components/MovieList';
+import { movies } from '../data/movies';
+
+const Home = () => {
+  return <MovieList movies={movies} />;
+};
+
+export default Home;

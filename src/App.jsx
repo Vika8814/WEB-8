c5a29e83d74e66deb6aef65b59426b49e568b977
Vika8814 lab1 <mvv081105@gmail.com>
2025-05-18 11:40:11 +0300

    Лабораторна 8: Оновлення компонента App для відображення списку фільмів

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.jsx
+import React from 'react';
+import MovieList from './components/MovieList';
+import { movies } from './data/movies';
+import styles from './styles/App.module.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.app}>
+      <h1 className={styles.title}>Кінотеатр</h1>
+      <MovieList movies={movies} />
     </div>
   );
 }
